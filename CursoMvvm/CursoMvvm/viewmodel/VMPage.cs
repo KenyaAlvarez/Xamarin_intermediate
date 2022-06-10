8f5af19660271b20abf06aa9a6e53f71f207d59c
Kenya <kalvarez@orthocana.com>
2022-06-10 16:52:58 -0500

    MVVVM Xamarin.

---
+++ CursoMvvm/CursoMvvm/viewmodel/VMPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CursoMvvm.viewmodel
+{
+    public class VMPage : BaseViewModel
+    {
+        #region VARIABLES
+        string _Mensaje;
+        #endregion
+
+        #region CONSTRUCTOR
+        public VMPage(INavigation navigation)
+        {
+            Navigation = navigation;
+        }
+        #endregion
+
+        #region OBJETOS
+        public string Mensaje
+        {
+            get { return _Mensaje; }
+            set { SetValue(ref _Mensaje, value); }
+        }
+        #endregion
+
+        #region PROCESOS
+        public async Task Alerta()
+        {
+            await DisplayAlert("Titutlo", Mensaje, "OK");
+        }
+
+        public void ProcesoSimple()
+        {
+            
+        }
+        #endregion
+
+        #region COMANDOS
+        public ICommand Alertacommand => new Command(async () => await Alerta());
+        public ICommand ProcesoSimplecommand => new Command(ProcesoSimple);
+        #endregion
+    }
+}
+
---
+++ CursoMvvm/CursoMvvm/views/Page.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CursoMvvm.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoMvvm.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page : ContentPage
+    {
+        public Page()
+        {
+            InitializeComponent();
+            BindingContext = new VMPage(Navigation);
+        }
+    }
+}
